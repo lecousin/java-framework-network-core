fac3458d188b6738fd95f7955b851c808da8d1ce
Guillaume Le Cousin <guillaume@lecousin.net>
2020-03-01 08:20:02 +0100

    fix TestNetUtil

--- src/test/java/net/lecousin/framework/network/tests/TestNetUtil.java
+++ src/test/java/net/lecousin/framework/network/tests/TestNetUtil.java
@@ -14,7 +14,7 @@ public class TestNetUtil extends LCCoreAbstractTest {
 	@Test
 	public void test() throws Exception {
 		NetUtil.getAllIPs();
-		Assert.assertNull(NetUtil.getIPsFromMAC(new byte[] { 1, 2, 3, 4, 5, 6 }));
+		Assert.assertEquals(0, NetUtil.getIPsFromMAC(new byte[] { 1, 2, 3, 4, 5, 6 }).size());
 		NetworkInterface i = NetworkInterface.getNetworkInterfaces().nextElement();
 		NetUtil.getIPsFromMAC(i.getHardwareAddress());
 	}
